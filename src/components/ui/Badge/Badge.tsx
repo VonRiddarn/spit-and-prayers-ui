0b834923701e37f028a30ef9a4ed169393d70a81
VonRiddarn <vonriddarn@gmail.com>
2025-02-13 20:59:11 +0100

    feat: changed Badge to be of type span

--- src/components/ui/Badge/Badge.tsx
+++ src/components/ui/Badge/Badge.tsx
@@ -22,7 +22,7 @@ const Badge = ({
 	if (zeroBehavior === "no-render" && invalidAmount) return;
 
 	return (
-		<div
+		<span
 			className="Badge"
 			style={{
 				backgroundColor: color,
@@ -39,7 +39,7 @@ const Badge = ({
 				</span>
 			</span>
 			<span>{title}</span>
-		</div>
+		</span>
 	);
 };
 
