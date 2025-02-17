c1f61660fbc026fc387f8afa5daa3d9d64d9ac27
VonRiddarn <vonriddarn@gmail.com>
2025-02-17 09:57:39 +0100

    feat: added prop drilling to pass down value, placeholder and onChange in LegendInputField

--- src/components/ui/LegendInputField/LegendInputField.tsx
+++ src/components/ui/LegendInputField/LegendInputField.tsx
@@ -3,13 +3,16 @@ import "./LegendInputField.scss";
 type LegendInputFieldProps = {
 	title: string;
 	type: "email" | "text" | "password";
+	value?: string;
+	placeholder?: string;
+	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const LegendInputField = ({ title, type }: LegendInputFieldProps) => {
+const LegendInputField = ({ title, type, placeholder, value, onChange }: LegendInputFieldProps) => {
 	return (
 		<fieldset className="LegendInputField">
 			<legend>{title}</legend>
-			<input type={type} />
+			<input type={type} placeholder={placeholder} value={value} onChange={onChange} />
 		</fieldset>
 	);
 };
