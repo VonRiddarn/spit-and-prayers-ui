0ff302273a4eee0b38057c63ca04875381175ed7
VonRiddarn <vonriddarn@gmail.com>
2025-02-19 16:19:05 +0100

    feat: added mock cards

---
+++ src/data/mockCards.tsx
@@ -0,0 +1,32 @@
+export const mockCards = [
+	<article>
+		<div>
+			<h2>Test 1</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, commodi!</p>
+		</div>
+	</article>,
+	<article>
+		<div>
+			<h2>Test 2</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, commodi!</p>
+		</div>
+	</article>,
+	<article>
+		<div>
+			<h2>Test 3</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, commodi!</p>
+		</div>
+	</article>,
+	<article>
+		<div>
+			<h2>Test 4</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, commodi!</p>
+		</div>
+	</article>,
+	<article>
+		<div>
+			<h2>Test 5</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, commodi!</p>
+		</div>
+	</article>,
+];
